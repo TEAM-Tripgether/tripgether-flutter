c385a4127a30b88545928a42ccce3c875045aef9
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-10-09 03:07:05 +0900

    프로젝트 기본 설정 추가 필요 : feat : bundle id 변경 및 ios testflight deploy로직 추가 https://github.com/TEAM-Tripgether/tripgether-flutter/issues/1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 android {
-    namespace = "com.tripgether.app"
+    namespace = "com.tripgether.alom"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -27,7 +27,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.tripgether.app"
+        applicationId = "com.tripgether.alom"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
